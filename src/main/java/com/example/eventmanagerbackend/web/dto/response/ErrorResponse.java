91200cef433fc8647528c617a897a3bc56ec2cad
Vladimir <vova.kaverya@mail.ru>
2024-07-17 21:53:49 +0300

    project structure

---
+++ src/main/java/com/example/eventmanagerbackend/web/dto/response/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.eventmanagerbackend.web.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+
+}
---
+++ src/main/java/com/example/eventmanagerbackend/web/handler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.example.eventmanagerbackend.web.handler;
+
+import com.example.eventmanagerbackend.exception.AccessDeniedException;
+import com.example.eventmanagerbackend.exception.AlreadyExistsException;
+import com.example.eventmanagerbackend.exception.EntityNotFoundException;
+import com.example.eventmanagerbackend.web.dto.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalControllerExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ErrorResponse("File too large!"));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFound(EntityNotFoundException ex) {
+        log.error("Error trying to get entity", ex);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> accessDenied(AccessDeniedException ex) {
+        log.error("Error trying to change entity", ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> alreadyExists(AlreadyExistsException ex) {
+        log.error("Error trying to create or update entity", ex);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> notValid(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> errorMessages = bindingResult.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        String errorMessage = String.join("; ", errorMessages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+}
