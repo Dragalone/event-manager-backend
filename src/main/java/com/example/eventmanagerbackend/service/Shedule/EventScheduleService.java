227445601971855064ae77dcb5a6b87728bcf12b
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2024-07-20 17:20:12 +0300

    SHEDULE!!!!!!!!!!!!

---
+++ src/main/java/com/example/eventmanagerbackend/service/Shedule/EventScheduleService.java
@@ -0,0 +1,49 @@
+package com.example.eventmanagerbackend.service.Shedule;
+
+import com.example.eventmanagerbackend.entity.Event;
+import com.example.eventmanagerbackend.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+@Service
+public class EventScheduleService {
+
+    private final EventRepository eventRepository;
+    @Autowired
+    public EventScheduleService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Scheduled(cron = "${interval-delete-event-cron}")
+    public void changeRegistrationStatus(){
+        LocalDate currentDate = LocalDate.now();
+
+        List<Event> events = StreamSupport
+                .stream(eventRepository.findAll().spliterator(), false)
+                .toList();
+
+        List<Event> updatedEvents = events.stream()
+                .peek(event -> {
+                    //  LocalDate eventEnd = event.getEndRegistration().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                    //   LocalDate eventStart = event.getStartRegistration().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                    LocalDate eventEnd = event.getDate().atZone(ZoneId.systemDefault()).toLocalDate();
+
+                    if (event.getRegOpen() && currentDate.isAfter(eventEnd)) {//!!!!!
+                        event.setRegOpen(false);
+                    } else if (!event.getRegOpen() && currentDate.isBefore(eventEnd)) {
+                        event.setRegOpen(true);
+                    }
+                })
+                .toList();
+        if(!updatedEvents.isEmpty())
+            eventRepository.saveAll(updatedEvents);
+
+    }
+
+}
