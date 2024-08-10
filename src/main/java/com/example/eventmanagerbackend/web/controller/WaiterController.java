e3e90e2a7531355936a87be0d416f4474762a145
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2024-08-10 21:35:16 +0300

    Waiters. DB Conflict I don`t know

--- src/main/java/com/example/eventmanagerbackend/web/controller/WaiterController.java
+++ src/main/java/com/example/eventmanagerbackend/web/controller/WaiterController.java
@@ -1,16 +1,17 @@
 package com.example.eventmanagerbackend.web.controller;
 
 import com.example.eventmanagerbackend.service.WaiterService;
+import com.example.eventmanagerbackend.web.dto.request.UpsertEventRequest;
+import com.example.eventmanagerbackend.web.dto.request.WaiterRequest;
+import com.example.eventmanagerbackend.web.dto.response.EventResponse;
 import com.example.eventmanagerbackend.web.dto.response.ModelListResponse;
 import com.example.eventmanagerbackend.web.dto.response.WaiterResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/waiters", produces = "application/json")
@@ -25,5 +26,11 @@ public class WaiterController {
     public ResponseEntity<ModelListResponse<WaiterResponse>> getAll(Pageable pageable)
     {
         return ResponseEntity.ok(waiterService.findAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<WaiterResponse> createWaiter(@RequestBody WaiterRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(waiterService.create(request));
     }
 }
